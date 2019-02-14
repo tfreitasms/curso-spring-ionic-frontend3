000e821b2e0aeb4453dd719a32da03a40b12983e
Thiago Freitas <tfreitas.ms@gmail.com>
2019-02-14 19:50:21 -0200

    Carregando imagens dos produtos

--- src/pages/produtos/produtos.ts
+++ src/pages/produtos/produtos.ts
@@ -1,3 +1,4 @@
+import { API_CONFIG } from './../../config/api.config';
 import { ProdutoService } from './../../services/domain/produto.service';
 import { ProdutoDTO } from './../../models/produto.dto';
 import { Component } from '@angular/core';
@@ -23,9 +24,21 @@ export class ProdutosPage {
     this.produtoService.findByCategoria(categoria_id)
         .subscribe(response => {
           this.items = response['content'];
+          this.loadImageUrls();
         },
         error => {});    
 
 }
+
+  loadImageUrls() {
+    for (var i=0; i<this.items.length; i++) {
+      let item = this.items[i];
+      this.produtoService.getSmallImageFromBucket(item.id)
+      .subscribe(response => {
+        item.imageUrl = `${API_CONFIG.bucketBaseUrl}/prod${item.id}-small.jpg`;
+      },
+      error => {});
+    }
+  }
 
 }
