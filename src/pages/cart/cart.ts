6f8f43d87e4d148c95b8da1085595d61c10cc2bf
tfreitasms <tfreitas.ms@gmail.com>
2019-02-17 23:37:32 -0300

    Ajustando pagina e funcionalidades do carrinho

--- src/pages/cart/cart.ts
+++ src/pages/cart/cart.ts
@@ -1,3 +1,4 @@
+import { ProdutoDTO } from './../../models/produto.dto';
 import { CartService } from './../../services/domain/cart.service';
 import { ProdutoService } from './../../services/domain/produto.service';
 import { CartItem } from './../../models/cart-item';
@@ -38,6 +39,26 @@ export class CartPage {
       },
       error => {});
     }
+  }
+
+  removeItem (produto: ProdutoDTO) {
+    this.items = this.cartService.removeProduto(produto).items;
+  }
+
+  increaseQuantity(produto: ProdutoDTO) {
+    this.items = this.cartService.increaseQuantity(produto).items;
+  }
+
+  decreaseQuantity(produto: ProdutoDTO) {
+    this.items = this.cartService.decreaseQuantity(produto).items;
+  }
+
+  total() : number {
+    return this.cartService.total();
+  }
+
+  goOn() {
+    this.navCtrl.setRoot('CategoriasPage');
   }
 
 }
